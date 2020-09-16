41ef831aea89b42dd69713ee613a1c6025746ca8
Marvin Sernee <mmhsernee@gmail.com>
2020-09-16 14:21:56 +0200

    Add files via upload

---
+++ train.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+import keras_preprocessing
+from keras_preprocessing import image
+from keras_preprocessing.image import ImageDataGenerator
+
+#Dataset directories
+train_dir = 'data/train'
+val_dir = 'data/test'
+
+#Image Datagenerators
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+  train_dir,
+  target_size=(48, 48),
+  class_mode='categorical'
+)
+
+validation_generator = val_datagen.flow_from_directory(
+  val_dir,
+  target_size=(48, 48),
+  class_mode='categorical'
+)
+
+# Convolutional Neural Network(CNN)
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(48, 48, 3)),
+  
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Dropout(0.5),
+
+  tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Dropout(0.5),
+
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(1024, activation='relu'),
+  tf.keras.layers.Dense(7, activation='softmax')
+])
+
+model.compile(
+  loss='categorical_crossentropy',
+  optimizer='adam',
+  metrics=['accuracy']
+)
+
+history = model.fit(
+  train_generator,
+  epochs=50,
+  validation_data=validation_generator,
+  verbose=1
+)
+
+model.save('model.h5')
