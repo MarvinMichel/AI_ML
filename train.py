949e2615b481192d071b16fa1e6ea03a51ef0d2f
MarvinMichel <mmhsernee@gmail.com>
2020-09-16 19:05:07 +0200

    New model

--- train.py
+++ train.py
@@ -14,32 +14,35 @@ val_datagen = ImageDataGenerator(rescale=1./255)
 train_generator = train_datagen.flow_from_directory(
   train_dir,
   target_size=(48, 48),
+  batch_size=64,
+  color_mode='grayscale',
   class_mode='categorical'
 )
 
 validation_generator = val_datagen.flow_from_directory(
   val_dir,
   target_size=(48, 48),
+  batch_size=64,
+  color_mode='grayscale',
   class_mode='categorical'
 )
 
 # Convolutional Neural Network(CNN)
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(48, 48, 3)),
-  
+  tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(48, 48, 1)),
   tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-  tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-  tf.keras.layers.MaxPooling2D(2, 2),
-  tf.keras.layers.Dropout(0.5),
+  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.Dropout(0.25),
 
   tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
-  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.MaxPooling2D(2,2),
   tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
-  tf.keras.layers.MaxPooling2D(2, 2),
-  tf.keras.layers.Dropout(0.5),
+  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.Dropout(0.25),
 
   tf.keras.layers.Flatten(),
   tf.keras.layers.Dense(1024, activation='relu'),
+  tf.keras.layers.Dropout(0.5),
   tf.keras.layers.Dense(7, activation='softmax')
 ])
 
@@ -51,7 +54,7 @@ model.compile(
 
 history = model.fit(
   train_generator,
-  epochs=50,
+  epochs=25,
   validation_data=validation_generator,
   verbose=1
 )
